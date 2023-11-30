f4d5cf9daa6655bac620dc4b13d730808ff92043
Anil Manchanda <amanchan@yahoo.com>
2023-11-29 20:01:19 -0600

    Added Title and description in routing calls. Provides better docs in OpenAPI and redoc

--- scrapper/app/main.py
+++ scrapper/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated
 from .scrapper import pages, feeds
 
 app=FastAPI(title="Web Scraping using FastAPI",
@@ -6,12 +7,17 @@ app=FastAPI(title="Web Scraping using FastAPI",
 
 
 @app.get("/pages")
-def home_pages(page: str | None = 'world'):
+def home_pages(page: Annotated[str | None,
+                            Query(title= "Show News Page Data for",
+                                  description="business, markets, legal, technology etc")] = 'world'):
     data = pages(page)
     return data
 
 
 @app.get("/news")
-def home_news(page: str | None = 'world', category: str | None = 'africa'):
+def home_news(page: str | None = 'world',
+              category: Annotated[str | None, Query(
+                  description="environment, energy etc in business page<br>africa, americas in world page"
+              )] = 'africa'):
     data = feeds(page, category)
     return data
